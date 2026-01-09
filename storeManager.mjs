8cb8d23b7a2590935fb7e910226ea70103e482e4
Matthew Gasbarra <mrgasbarra85@gmail.com>
2026-01-08 18:38:03 -0800

    adding storeManager.mjs to main

---
+++ storeManager.mjs
@@ -0,0 +1,9 @@
+import { addInventory, removeInventory, listInventory } from "./inventory.mjs";
+
+addInventory("Jersey");
+addInventory("Hat");
+addInventory("Shorts");
+addInventory("Beanie");
+removeInventory("Hat");
+removeInventory("Basketball");
+listInventory();
