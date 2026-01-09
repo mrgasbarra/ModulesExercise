a261961f9c7781963857a0803fb0fab0e3979b42
Matthew Gasbarra <mrgasbarra85@gmail.com>
2026-01-08 18:43:18 -0800

    adding Post.mjs to main and modified storeManager.mjs

---
+++ Post.mjs
@@ -0,0 +1,3 @@
+/*- Create `Post.mjs`, exporting a default `Post` class that includes a constructor for `title` 
+and `content`, and a `publish` method that logs the title and content to the console.*/
+
--- storeManager.mjs
+++ storeManager.mjs
@@ -1,3 +1,6 @@
+/*Implement storeManager.mjs that imports the functions from inventory.mjs to demonstrate adding a few items, 
+removing an item, and then listing the remaining items in the inventory.*/
+
 import { addInventory, removeInventory, listInventory } from "./inventory.mjs";
 
 addInventory("Jersey");
