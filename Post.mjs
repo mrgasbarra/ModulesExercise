383c513f76edcfd9fec26676fc38acf3c3e90de3
Matthew Gasbarra <mrgasbarra85@gmail.com>
2026-01-08 19:32:22 -0800

    updated Post.mjs

--- Post.mjs
+++ Post.mjs
@@ -1,3 +1,14 @@
 /*- Create `Post.mjs`, exporting a default `Post` class that includes a constructor for `title` 
 and `content`, and a `publish` method that logs the title and content to the console.*/
 
+export class Post {
+    constructor(title, content){
+        this.title = title;
+        this.content = content;
+    }
+
+    publish = () =>{
+        console.log(`Title:${this.title}`);
+        console.log(`Post: ${this.content}`);
+    }
+}
